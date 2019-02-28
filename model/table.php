6002daf3978e40c1b075eef3b0417d96a60b5e65
Komche <Adamoukomcheabdoularazak@gmail.com>
2019-02-28 10:39:59 +0100

    Etat initial avant la personnalisatio

---
+++ model/table.php
@@ -0,0 +1,148 @@
+<?php
+
+    include_once('../config/connexion.php');
+
+    $database = new Connexion();
+    $db = $database->getConnection();
+
+    $request_method = $_SERVER['REQUEST_METHOD'];
+
+    switch ($request_method) {
+        case 'GET':
+            if(!empty($_GET['id'])){
+                $id = intval($_GET['id']);
+                get_employees($id);
+            }else{
+                get_employees();
+            }
+            break;
+        
+        case 'POST':
+            $data = json_decode(file_get_contents('php://input'),true);
+            insert('vols',['ville_depart', 'ville_arriver', 'nb_heure_vols', 'prix'],$data);
+            break;
+
+        case 'PUT':
+            $id = intval($_GET['id']);
+            $data = json_decode(file_get_contents('php://input'),true);
+            update('vols',['ville_depart', 'ville_arriver', 'nb_heure_vols', 'prix'],$data, 'id', $id);
+            break;
+        case 'DELETE':
+            $id = intval($_GET['id']);
+            delete('vols', 'id', $id);
+            break;
+        
+        default:
+            header('HTTP/1.0 405 Method Not Allowed');
+            break;
+    }
+
+    function get_employees($id=null)
+    {
+        global $db;
+        $query = "SELECT * FROM vols ";
+        if ($id!=null) {
+            $query .= "WHERE id=:id LIMIT 1";
+            $req = $db->prepare($query);
+            $req->execute(['id'=>$id]);
+            if ($result = $req->fetch(PDO::FETCH_ASSOC)) {
+                header('Content-Type: application/json');
+                echo json_encode($result);
+            }
+        }else{
+            $req = $db->query($query);
+            if ($result = $req->fetchAll(PDO::FETCH_ASSOC)) {
+                header('Content-Type: application/json');
+                echo json_encode($result);
+            }
+        }       
+        
+        
+    }
+
+    function insert($table, $fields = [], $values = [])
+    {
+        global $db;
+        if (count($fields) > 0) {
+            $total = count($fields) - 1;
+            $sql = "INSERT INTO $table(";
+            foreach ($fields as $key => $field) {
+                if ($total != $key) {
+                    $sql .= $field . ", ";
+                } else {
+                    $sql .= $field . ") ";
+                }
+            }
+            $sql .= "VALUES(";
+            foreach ($fields as $key => $field) {
+                if ($total != $key) {
+                    $sql .= ":$field, ";
+                } else {
+                    $sql .= ":$field)";
+                }
+            }
+
+            $req = $db->prepare($sql);
+            if ($req->execute($values)) {
+                $result = array("status"=>1,
+                                "message"=> "Enregistrement ajouté avec succès");
+            } else {
+                $result = array("status"=>0,
+                                "message"=> "Enregistrement échouer");
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($result);
+            
+        }
+    }
+
+    function update($table, $fields = [], $values = [], $id, $id_val)
+    {
+        global $db;
+        if (count($fields) > 0) {
+            $total = count($fields) - 1;
+            $sql = "UPDATE $table SET ";
+            foreach ($fields as $key => $field) {
+                if ($total != $key) {
+                    $sql .= "$field=:$field, ";
+                } else {
+                    $sql .= "$field=:$field ";
+                }
+            }
+            $sql .= "WHERE $id=:$id";
+            
+            $values[$id] = $id_val;
+            //echo($sql); print_r($values); die();
+
+            $req = $db->prepare($sql);
+            if ($req->execute($values)) {
+                $result = array("status"=>1,
+                                "message"=> "Enregistrement modifier avec succès");
+            } else {
+                $result = array("status"=>0,
+                                "message"=> "modification échouer");
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($result);
+            
+        }
+    }
+
+    function delete($table, $id, $id_val)
+    {
+        global $db;
+        $sql = "DELETE FROM $table WHERE $id=$id_val";
+
+        if ($db->exec($sql)) {
+            $result = array("status"=>1,
+                            "message"=> "Enregistrement supprimer avec succès");
+        } else {
+            $result = array("status"=>0,
+                            "message"=> "suppression échouer");
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($result);
+    }
